60e09dcf10f5ff0b5541d965ec067a8105226a76
Martin Lange <ml_ks@web.de>
2023-06-09 16:05:55 +0200

    Use density instead of distribution, add different epsilons for pearson graph.

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+import itertools
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-EPS = 4.0
+EPS = 1.0
 
 
-def F(x, epsilon=EPS):
+def f(x):
+    return np.exp(-np.abs(x) * EPS) * (EPS / 2.0)
+
+
+def F(x):
     if x < 0.0:
-        return 0.5 * np.exp(epsilon * x)
+        return 0.5 * np.exp(EPS * x)
     else:
-        return 1.0 - 0.5 * np.exp(-epsilon * x)
+        return 1.0 - 0.5 * np.exp(-EPS * x)
 
 
 def prob_for_output_given_x0(o, x0, data_prob):
     prob_sum = 0
 
     for x1 in [0, 1]:
-        prob_sum += data_prob[(x0, x1)] / (data_prob[(x0, 0)] + data_prob[(x0, 1)]) * F(o - x0 - x1)
+        prob_sum += data_prob[(x0, x1)] / (data_prob[(x0, 0)] + data_prob[(x0, 1)]) * f(o - x0 - x1)
 
     return prob_sum
 
@@ -48,11 +54,31 @@ def get_uncorrelated_data(prob_x0, prob_x1):
     return data_prob
 
 
-if __name__ == '__main__':
+def generate_uncorrelated_data(n, seed=123):
+    rng = np.random.default_rng(seed=seed)
 
+    marginals = []
+    for i in range(n):
+        p0 = rng.uniform(0.1, 0.9)
+        p1 = 1 - p0
+        marginals.append({0: p0, 1: p1})
+
+    data_prob = {}
+
+    lists = [[0, 1] for _ in range(n)]
+    for a in itertools.product(*lists):
+        p = 1.0
+        for i, marginal in enumerate(marginals):
+            p *= marginal[a[i]]
+        data_prob[a] = p
+
+    return data_prob
+
+
+if __name__ == '__main__':
     # correlated case
 
-    data_prob = get_correlated_data(pearson_correlation_coefficient=1)
+    data_prob = get_correlated_data(pearson_correlation_coefficient=1.0)
     print(data_prob)
 
     # probability that the output is less than max_output
@@ -76,9 +102,15 @@ if __name__ == '__main__':
 
 
     ps = np.linspace(-1, 1, 100)
-    apgs_pearson = [associative_privacy_budget(-50, get_correlated_data(p)) for p in ps]
-    plt.plot(ps, 100*[EPS], label='DP epsilon')
-    plt.plot(ps, apgs_pearson, label='associative epsilon for pearson coefficient')
+    apgs_pearson_1 = [associative_privacy_budget(-50, get_correlated_data(p)) / EPS for p in ps]
+    EPS = 3.0
+    apgs_pearson_3 = [associative_privacy_budget(-50, get_correlated_data(p)) / EPS for p in ps]
+    EPS = 10.0
+    apgs_pearson_10 = [associative_privacy_budget(-50, get_correlated_data(p)) / EPS for p in ps]
+    #plt.plot(ps, 100*[EPS], label='DP epsilon')
+    plt.plot(ps, apgs_pearson_1, label='associative privacy loss / epsilon (epsilon = 1)')
+    plt.plot(ps, apgs_pearson_3, label='associative privacy loss / epsilon (epsilon = 3)')
+    plt.plot(ps, apgs_pearson_10, label='associative privacy loss / epsilon (epsilon = 10)')
 
     plt.legend()
     plt.show()
